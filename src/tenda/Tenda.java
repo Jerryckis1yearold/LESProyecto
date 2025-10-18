63c877224fecd430e2dff31a6a7a6ae063247140
david <david@David>
2025-10-18 15:09:01 -0600

    B

--- src/tenda/Tenda.java
+++ src/tenda/Tenda.java
@@ -14,7 +14,7 @@ public class Tenda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Tenda");
+        System.out.println("ANtuna");
         // TODO code application logic here
     }
     
